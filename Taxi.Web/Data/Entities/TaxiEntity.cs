755a475952fb9cb075d2d413073e23b8c8db6a18
JULIAN ANDRES OROZCO GARCIA <julianorozco236333@correo.itm.edu.co>
2020-02-15 13:28:16 -0500

    ACTUALIZACIONES CLASE SEMANA2

---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "The field {0} must stars wth three three characters and three numbers.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+
+        public String Plaque { get; set; }
+
+    }
+}
