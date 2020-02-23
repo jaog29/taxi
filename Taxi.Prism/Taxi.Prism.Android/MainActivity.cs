ff3a0744543bbbb6447ccf03cfaf45872632c0dc
JULIAN ANDRES OROZCO GARCIA <julianorozco236333@correo.itm.edu.co>
2020-02-23 11:55:35 -0500

    Commit semana 3 clase 2 final falta arreglar el mapa

--- Taxi.Prism/Taxi.Prism.Android/MainActivity.cs
+++ Taxi.Prism/Taxi.Prism.Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
 using Prism;
 using Prism.Ioc;
 
@@ -15,10 +17,17 @@ namespace Taxi.Prism.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
+            CrossCurrentActivity.Current.Init(this, bundle);
+                
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
         }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
     }
 
     public class AndroidInitializer : IPlatformInitializer
